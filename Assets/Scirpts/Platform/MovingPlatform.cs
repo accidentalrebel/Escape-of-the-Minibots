2d79274605b23daebd84ccb6794a4d6fd3b266a9
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-05 03:00:01 +0800

    * Added a moving platform that can be jumped on and ridden

---
+++ Assets/Scirpts/Platform/MovingPlatform.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+    	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Translate(Vector3.right * Time.deltaTime);   
+	}
+}

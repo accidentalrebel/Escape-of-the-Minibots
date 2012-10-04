ed0f66cbd7c9f46e99dfaa9b2c3a8f66c61e9b3e
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-05 03:22:38 +0800

    * Moving platform now moves from starting position to end position

--- Assets/Scirpts/Platform/MovingPlatform.cs
+++ Assets/Scirpts/Platform/MovingPlatform.cs
@@ -2,9 +2,51 @@ using UnityEngine;
 using System.Collections;
 
 public class MovingPlatform : MonoBehaviour {
-    	
+
+    public enum Direction { Left, Right };
+
+    public Vector2 startingPosition;
+    public Vector2 endingPosition;
+    public float speed = 1;
+    public Direction startingDirection = Direction.Right;
+    private Direction direction;
+
+
+    void Start()
+    {
+        if (startingPosition == Vector2.zero)
+            startingPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        if (endingPosition == null)
+            Debug.LogError("EndingPosition not specified!");
+
+        direction = startingDirection;
+    }
+
 	// Update is called once per frame
 	void Update () {
-        gameObject.transform.Translate(Vector3.right * Time.deltaTime);   
+
+        // Have we reached the ending position?
+        if ((direction == Direction.Right && transform.position.x >= endingPosition.x)
+            || ( direction == Direction.Left && transform.position.x <= startingPosition.x))
+        {
+            ChangeDirection();
+        }
+
+        if (direction == Direction.Right)
+        {
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        else
+        {
+            gameObject.transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
 	}
+
+    private void ChangeDirection()
+    {
+        if (direction == Direction.Right)
+            direction = Direction.Left;
+        else
+            direction = Direction.Right;
+    }
 }

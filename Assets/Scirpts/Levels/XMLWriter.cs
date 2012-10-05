46d3f0ccad3a1adbf8d61fc5fab4c05ed218a1da
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-06 05:05:51 +0800

    * If XML file does not exist. A new file is created.

--- Assets/Scirpts/Levels/XMLWriter.cs
+++ Assets/Scirpts/Levels/XMLWriter.cs
@@ -7,26 +7,38 @@ public class XMLWriter : MonoBehaviour {
 
     void Start()
     {
-        string filepath = Application.dataPath + @"/Levels/1.xml";
+        string filepath = Application.dataPath + @"/Levels/test.xml";
         Debug.Log(filepath);
 
         XmlDocument xmlDoc = new XmlDocument();
-       
-        if (File.Exists(filepath))
+
+        // If file does not exist. Create the xml file.
+        if (!File.Exists(filepath))
         {
-            Debug.Log("loaded");
-            xmlDoc.Load(filepath);
-            XmlElement elemRoot = xmlDoc.DocumentElement;
-            elemRoot.RemoveAll();                               // Remove all inside the transforms node
-            XmlElement elemNew = xmlDoc.CreateElement("rotation");  // Create the rotation node
+            Debug.LogWarning("File does not exist. Creating file");
 
-            XmlElement rotationX = xmlDoc.CreateElement("x");   // Create the rotation x node
-            rotationX.InnerText = "1";
-
-            elemNew.AppendChild(rotationX);                     // Make the rotation node the parent
-            elemRoot.AppendChild(elemNew);                      // Make the transform node the parent
-
-            xmlDoc.Save(filepath);
+            FileStream fs = File.Create(filepath);
+            StreamWriter writer = new StreamWriter(fs);
+            writer.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            writer.WriteLine("<tiles>");
+            writer.Write("</tiles>");
+            writer.Flush();
+            fs.Close();
         }
+
+        Debug.Log("loaded");
+        xmlDoc.Load(filepath);
+        XmlElement elemRoot = xmlDoc.DocumentElement;
+        elemRoot.RemoveAll();                               // Remove all inside the transforms node
+        XmlElement elemNew = xmlDoc.CreateElement("rotation");  // Create the rotation node
+        //XmlAttribute 
+
+        XmlElement rotationX = xmlDoc.CreateElement("x");   // Create the rotation x node
+        rotationX.InnerText = "1";
+
+        elemNew.AppendChild(rotationX);                     // Make the rotation node the parent
+        elemRoot.AppendChild(elemNew);                      // Make the transform node the parent
+
+        xmlDoc.Save(filepath);
     }
 }

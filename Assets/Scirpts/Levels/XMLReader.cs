c7bc16e4a9efc136aa06558784a902923ccae959
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-06 04:21:51 +0800

    * Created an xml reader script

---
+++ Assets/Scirpts/Levels/XMLReader.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class XMLReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        XmlReader reader = XmlReader.Create(@"Assets\Levels\1.xml");
+        Debug.Log(reader);
+
+        while (reader.Read())
+        {
+            if (reader.IsStartElement("tile"))
+            {
+                Debug.Log(reader.GetAttribute("x"));                   
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

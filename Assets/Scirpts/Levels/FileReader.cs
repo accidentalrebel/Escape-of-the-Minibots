499bb87b3f540f9c39de8347cd1605003b2273e4
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-06 01:43:56 +0800

    * Can now read a text file

---
+++ Assets/Scirpts/Levels/FileReader.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public static class FileReader
+{
+    public static string GetData(string filename)
+    {
+        StreamReader file = new StreamReader(Application.dataPath + @"\Levels\" + filename + ".txt");
+        string fileContent = file.ReadToEnd();
+        file.Close();
+
+        return fileContent;
+    }
+}
---
+++ Assets/Scirpts/Levels/LevelLoader.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(FileReader))]
+public class LevelLoader : MonoBehaviour {
+        
+	// Use this for initialization
+	void Start () {
+        BuildLevel(GetLevelData("1"));
+	}
+
+    private string GetLevelData(string levelLabel)
+    {       
+        return FileReader.GetData(levelLabel);
+    }
+
+    private void BuildLevel(string levelData)
+    {
+        Debug.Log("levelData is " + levelData);        
+    }	
+}

5e23c18b7a3c194f6e73361f0a10c7e2105a0ec2
ARebel <ARebel@ARebel-Laptop>
2012-10-09 13:00:41 +0800

    * Fixed problem where levelObject startingPos is not saved

--- Assets/Scirpts/Objects/LevelObject.cs
+++ Assets/Scirpts/Objects/LevelObject.cs
@@ -5,14 +5,17 @@ public class LevelObject : MonoBehaviour {
 
     protected Tile theTile;
 	internal Vector3 startingPos;
-
+	
+	void Awake()
+	{
+		startingPos = gameObject.transform.position;	
+	}
+	
     protected void Start()
     {
         theTile = gameObject.GetComponent<Tile>();
         if (theTile == null)
             Debug.LogError("theTile can not be found!");
-		
-		startingPos = gameObject.transform.position;
     }
 
     virtual internal void Use()

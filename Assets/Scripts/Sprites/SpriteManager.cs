e6d97db7a95abe9fa994afa32792f63852388d94
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-16 08:57:33 +0800

    * Added level 13 introducing switches

---
+++ Assets/Scripts/Sprites/SpriteManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class SpriteManager : MonoBehaviour {
+
+    Renderer theRenderer;
+
+	// Use this for initialization
+	void Start () {
+        theRenderer = gameObject.GetComponent<Renderer>().renderer;
+        if (theRenderer == null)
+            Debug.LogError("theRenderer is not found!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

a180cf7897694c8901b0738431d009817e518ff2
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-16 17:39:54 +0800

    * Can now specify animation sets that you can set so that the animation method knows what animationSet to play

--- Assets/Scripts/Sprites/SpriteManager.cs
+++ Assets/Scripts/Sprites/SpriteManager.cs
@@ -9,15 +9,22 @@ public class SpriteManager : MonoBehaviour {
     public float animationSpeed = 1;
     public bool enableAnimation = true;
     int totalNumberOfFrames;
+    string currentAnimation;
+
     Dictionary<int, Vector2> animationFrames = new Dictionary<int, Vector2>();
+    Dictionary<string, int[]> animationSets = new Dictionary<string, int[]>();
 
 	// Use this for initialization
 	void Start () {
+        currentAnimation = "default";
+
         theRenderer = gameObject.GetComponent<Renderer>().renderer;
         if (theRenderer == null)
             Debug.LogError("theRenderer is not found!");
                 
         Initialize(4, 4);
+        CreateAnimation("walking", new int[] {1,3});
+        SetCurrentAnimation("walking");
         StartCoroutine("Animate");
 	}
 
@@ -29,12 +36,14 @@ public class SpriteManager : MonoBehaviour {
         totalNumberOfFrames = numOfVerticalFrames * numOfVerticalFrames;
 
         int frameIndex = 1;
+        int[] defaultFrameSet = new int[totalNumberOfFrames+1];
         Vector2 currentOffset = new Vector2(0, 1f-offsetDifference.y);
         
         while ( frameIndex <= totalNumberOfFrames )
-        {           
+        {   
             animationFrames.Add(frameIndex, currentOffset);
-            frameIndex++;            
+            defaultFrameSet[frameIndex-1] = frameIndex;
+            frameIndex++;
             currentOffset.x += offsetDifference.x;
 
             // If we exceed the xoffset
@@ -51,26 +60,36 @@ public class SpriteManager : MonoBehaviour {
                 }
             }
         }
+
+        // We create the default animation
+        CreateAnimation("default", defaultFrameSet);
+        SetCurrentAnimation("default");
     }
 
-    void SetAnimation(string animationName, int[] frameSet)
+    private void SetCurrentAnimation(string nameOfAnimationSet)
     {
+        currentAnimation = nameOfAnimationSet;
+    }
 
+    void CreateAnimation(string animationName, int[] frameSet)
+    {
+        animationSets.Add(animationName, frameSet);
     }
 
     IEnumerator Animate()
     {
         int currentFrame = 1;
+        int[] animationFrameSets = animationSets[currentAnimation];        
 
         while (enableAnimation)
         {
-            theRenderer.material.SetTextureOffset("_MainTex", animationFrames[currentFrame]);
+            theRenderer.material.SetTextureOffset("_MainTex", animationFrames[animationFrameSets[currentFrame - 1]]);
 
             yield return new WaitForSeconds(animationSpeed);
             currentFrame++;
 
             // If we reached the end, go to the first frame
-            if (currentFrame > totalNumberOfFrames)
+            if (currentFrame > animationFrameSets.Length)
             {
                 currentFrame = 1;
             }

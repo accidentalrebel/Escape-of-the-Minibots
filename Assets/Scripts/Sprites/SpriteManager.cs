b9ceb3f0085561e9cc17b628cb2ff9a81d0102af
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-16 11:15:17 +0800

    * Basic animation system set up

--- Assets/Scripts/Sprites/SpriteManager.cs
+++ Assets/Scripts/Sprites/SpriteManager.cs
@@ -5,16 +5,34 @@ using System.Collections;
 public class SpriteManager : MonoBehaviour {
 
     Renderer theRenderer;
+    public float animationSpeed = 1;
+    public bool enableAnimation = true;
 
 	// Use this for initialization
 	void Start () {
         theRenderer = gameObject.GetComponent<Renderer>().renderer;
         if (theRenderer == null)
             Debug.LogError("theRenderer is not found!");
+
+        StartCoroutine("Animate");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    IEnumerator Animate()
+    {
+        Vector2 newOffset = new Vector2();
+        while (enableAnimation)
+        {
+            newOffset = theRenderer.material.GetTextureOffset("_MainTex");
+            newOffset.x += 0.25f;
+
+            // If we exceed the offset
+            if ( newOffset.x >= 1 )
+                newOffset.x = 0;    // Go back to the first frame
+
+            theRenderer.material.SetTextureOffset
+                ("_MainTex", newOffset);
+
+            yield return new WaitForSeconds(animationSpeed);            
+        }
+    }
 }

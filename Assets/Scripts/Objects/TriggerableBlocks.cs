d7e6663d9dcd122742c78c4ac7650113812b6d96
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-18 00:23:44 +0800

    * Can now manipulate the "width" and "height" properties of triggerableBlocks through the level Editor

--- Assets/Scripts/Objects/TriggerableBlocks.cs
+++ Assets/Scripts/Objects/TriggerableBlocks.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 //[ExecuteInEditMode]
 public class TriggerableBlocks : LevelObject {
@@ -7,6 +8,31 @@ public class TriggerableBlocks : LevelObject {
     public Vector2 blockSize;
     public bool isHidden = false;
     private bool startingIsHidden = false;
+    private List<GameObject> childTiles = new List<GameObject>();
+
+    string blockWidth = "1";
+    string BlockWidth
+    {
+        get { return blockWidth; }
+        set { blockWidth = value; SetBlockSizeFromString(blockWidth, blockHeight); }
+    }
+
+    string blockHeight = "1";
+    string BlockHeight
+    {
+        get { return blockHeight; }
+        set { blockHeight = value; SetBlockSizeFromString(blockWidth, blockHeight); }
+    }
+
+    private void SetBlockSizeFromString(string theBlockWidth, string theBlockHeight)
+    {
+        float floatBlockWidth = 0, floatBlockHeight = 0;
+        float.TryParse(theBlockWidth, out floatBlockWidth);
+        float.TryParse(theBlockHeight, out floatBlockHeight);
+
+        blockSize = new Vector2(floatBlockWidth, floatBlockHeight);
+        UpdateBlockSize(blockSize);
+    }    
 
 	// Use this for initialization
 	protected new void Start () {
@@ -23,13 +49,14 @@ public class TriggerableBlocks : LevelObject {
 
     private void GenerateTiles()
     {
+        childTiles.Clear();
         for (int yCoord = 0; yCoord < blockSize.y; yCoord++)
         {
             for (int xCoord = 0; xCoord < blockSize.x; xCoord++)
             {
                 // We just skip to the next xCoord as we already have a block present
-                if (yCoord == 0 && xCoord == 0)
-                    xCoord++;
+                //if (yCoord == 0 && xCoord == 0)
+                //    xCoord++;
 
                 GameObject newTile 
                     = (GameObject)Instantiate(Resources.Load(@"Prefabs/pfTriggerableTile"));
@@ -37,8 +64,17 @@ public class TriggerableBlocks : LevelObject {
                     = new Vector3(transform.position.x + xCoord
                         , transform.position.y + yCoord, 0);
                 newTile.transform.parent = this.transform;
+                childTiles.Add(newTile);
             }
         }
+    }
+
+    private void UpdateBlockSize(Vector2 newBlockSize)
+    {
+        blockSize = newBlockSize;
+
+        DestroyChildTiles();
+        GenerateTiles();
     }
 
     override internal void Use()
@@ -53,19 +89,27 @@ public class TriggerableBlocks : LevelObject {
 
     private void SetStatusOfChildTiles()
     {
-        foreach (Transform child in transform)
+        foreach (GameObject child in childTiles)
         {
             if (isHidden)
             {
-                child.gameObject.collider.enabled = false;
-                child.gameObject.GetComponent<GraphicHandler>().theRenderer.enabled = false;
+                child.collider.enabled = false;
+                child.GetComponent<GraphicHandler>().theRenderer.enabled = false;
             }
             else
             {
-                child.gameObject.collider.enabled = true;
-                child.gameObject.GetComponent<GraphicHandler>().theRenderer.enabled = true;
+                child.collider.enabled = true;
+                child.GetComponent<GraphicHandler>().theRenderer.enabled = true;
             }
         }      
+    }
+
+    private void DestroyChildTiles()
+    {
+        foreach (GameObject child in childTiles)
+        {
+            GameObject.Destroy(child);
+        }
     }
 
     void OnDrawGizmos()
@@ -85,5 +129,17 @@ public class TriggerableBlocks : LevelObject {
     {
         isHidden = startingIsHidden;
         SetStatusOfChildTiles();  
+    }
+
+    // ************************************************************************************
+    // OBJECT EDITING
+    // ************************************************************************************
+    internal override void GetEditableAttributes()
+    {
+        GUI.Label(new Rect((Screen.width / 2) - 140, (Screen.height / 2) - 110, 50, 20), "Width");
+        BlockWidth = GUI.TextField(new Rect((Screen.width / 2) - 90, (Screen.height / 2) - 110, 100, 20), BlockWidth);
+
+        GUI.Label(new Rect((Screen.width / 2) - 140, (Screen.height / 2) - 80, 50, 20), "Height");
+        BlockHeight = GUI.TextField(new Rect((Screen.width / 2) - 90, (Screen.height / 2) - 80, 100, 20), BlockHeight);
     }
 }

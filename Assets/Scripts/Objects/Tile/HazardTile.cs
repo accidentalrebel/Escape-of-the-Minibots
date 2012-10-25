1512e611db7fa4839d89d23994f5800ae9cef34a
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-26 00:30:23 +0800

    Backed out changeset: c4bded8e524a

--- Assets/Scripts/Objects/Tile/HazardTile.cs
+++ Assets/Scripts/Objects/Tile/HazardTile.cs
@@ -3,6 +3,20 @@ using System.Collections;
 
 public class HazardTile : Tile {
 
+    DynamicSizeObject dynamicSizeComponent;
+
+    void Start()
+    {
+        dynamicSizeComponent = gameObject.GetComponent<DynamicSizeObject>();
+        if (dynamicSizeComponent == null)
+        {
+            Debug.LogError("dynamicSizeComponent not specified");
+            return;
+        }
+
+        dynamicSizeComponent.Initialize(Registry.prefabHandler.pfHazard);
+    }
+
     void OnTriggerEnter(Collider other) 
     {        
         if (other.tag == "Player")

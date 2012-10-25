b50bd3037345f991e56ba21d826ecac3dc8f93af
Karlo Licudine <karlo@accidentalrebel.com>
2012-10-25 23:42:53 +0800

    * Created a Triggerable Hazard script * The initialize method in HazardTile script has been moved to the triggerable hazard script * The triggerableHazard script initializes the dynamic size object script

---
+++ Assets/Scripts/Objects/TriggerableHazard.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(DynamicSizeObject))]
+public class TriggerableHazard : MonoBehaviour {
+
+    DynamicSizeObject dynamicSizeComponent;
+
+    void Start()
+    {
+        dynamicSizeComponent = gameObject.GetComponent<DynamicSizeObject>();
+        if (dynamicSizeComponent == null)
+        {
+            Debug.LogError("dynamicSizeComponent not specified");
+            return;
+        }
+
+        dynamicSizeComponent.Initialize(Registry.prefabHandler.pfHazard);
+    }
+}

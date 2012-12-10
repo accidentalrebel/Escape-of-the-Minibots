5ce8f5d77d2cf6d2bf76862b86154e2f5dc24ccb
Karlo Licudine <karlo@accidentalrebel.com>
2012-12-11 03:37:36 +0800

    * When level is completed, the level recap menu is shown

---
+++ Assets/Scripts/GUI/MenuLevelRecap.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class MenuLevelRecap : MonoBehaviour {
+
+    bool isVisible = false;
+    public int menuWidth = 400;
+    public int menuHeight = 400;
+
+	// Use this for initialization
+	void Start () {
+        Registry.eventDispatcher.ELevelCompleted += ShowMenu;
+	}
+
+    void ShowMenu()
+    {
+        isVisible = true;
+    }
+
+    void OnGUI()
+    {
+        if (isVisible)
+        {
+            GUI.Box(new Rect
+                ( (Screen.width / 2) - (menuWidth / 2)
+                , (Screen.height / 2) - (menuHeight / 2)
+                , menuWidth, menuHeight), "Test");
+        }
+    }
+}

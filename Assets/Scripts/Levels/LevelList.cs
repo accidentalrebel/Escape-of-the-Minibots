5534b4039ffb60c11069c88aa7b70ae3ec08227a
Karlo Licudine <karlo@accidentalrebel.com>
2012-12-11 15:35:13 +0800

    * Created LevelList.cs that gets a list of all levels in the current map directory

---
+++ Assets/Scripts/Levels/LevelList.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class LevelList : MonoBehaviour {
+
+    List<string> levelList = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+        string directoryPath = Application.dataPath + @"/Resources/Levels/";
+        DirectoryInfo directory = new DirectoryInfo(directoryPath);
+        FileInfo[] info = directory.GetFiles("*.xml");
+
+        foreach (FileInfo file in info)
+        {
+            levelList.Add(file.Name.Substring(0, file.Name.Length - file.Extension.Length));
+        }
+
+        levelList.Sort();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

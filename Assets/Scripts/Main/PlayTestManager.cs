df06ee5d55b32ecc4228a47e63ee2473c5fa6da4
Karlo Licudine <karlo@accidentalrebel.com>
2012-12-17 23:02:02 +0800

    * Initial setup that lets the game send a text file to dropbox

---
+++ Assets/Scripts/Main/PlayTestManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayTestManager : MonoBehaviour {
+
+    void Awake()
+    {
+        StartCoroutine("UploadData");
+    }
+
+    public IEnumerator UploadData()
+    {
+        WWWForm wwwForm = new WWWForm();
+        wwwForm.AddField("replayData", "replay data");
+        wwwForm.AddField("timeStamp", "09/26/1986");
+        wwwForm.AddField("user", "user name");
+        WWW www = new WWW("http://www.accidentalrebel.com/minibots/playtestmailer.php", wwwForm);
+        yield return www;
+        Debug.Log("Uploaded replay data!");
+    }
+}

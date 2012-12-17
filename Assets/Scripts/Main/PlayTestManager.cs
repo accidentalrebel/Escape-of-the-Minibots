8c779170c3f60d9e2aeb45a1af33b70e736b44f8
Karlo Licudine <karlo@accidentalrebel.com>
2012-12-18 00:45:09 +0800

    * All data manipulatiopn is now done in unity instead of the php script * Changed how the php script accepts values.

--- Assets/Scripts/Main/PlayTestManager.cs
+++ Assets/Scripts/Main/PlayTestManager.cs
@@ -13,19 +13,20 @@ public class PlayTestManager : MonoBehaviour {
     {
         string replayData = Registry.replayManager.GetReplayDataString();
         string username = "Karlo";
-        string timeStamp = DateTime.Now.ToShortDateString() + "@" + DateTime.Now.Hour + "@" + DateTime.Now.Minute;
-        string fileData = Registry.map.currentLevel + "^" + username + "^" + replayData;        
-        StartCoroutine(UploadData(fileData, username, timeStamp));
+        string timeStamp = DateTime.Now.ToShortDateString() + "-" + DateTime.Now.Hour.ToString("00") + DateTime.Now.Minute;
+        string fileData = Registry.map.currentLevel + "^" + username + "^" + replayData;
+        string level = Registry.map.currentLevel;
+        StartCoroutine(UploadData(level, fileData, username, timeStamp));
     }
 
-    public IEnumerator UploadData(string fileData, string username, string timeStamp)
+    public IEnumerator UploadData(string level, string fileData, string username, string timeStamp)
     {
         Debug.Log("sending " + fileData + "-" + timeStamp + "-" + username);
 
         WWWForm wwwForm = new WWWForm();
+        wwwForm.AddField("emailSubject", level + "-" + username + "-" + timeStamp);
+        wwwForm.AddField("emailTxt", level + "-" + username + "-" + timeStamp);
         wwwForm.AddField("fileData", fileData);
-        wwwForm.AddField("timeStamp", timeStamp);
-        wwwForm.AddField("user", username);
         WWW www = new WWW("http://www.accidentalrebel.com/minibots/playtestmailer.php", wwwForm);
         yield return www;
         Debug.Log("Uploaded replay data!");

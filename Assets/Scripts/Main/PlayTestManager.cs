9d223401edb393da2b6aefa2d38a6fe87d792d1b
Karlo Licudine <karlo@accidentalrebel.com>
2012-12-19 14:36:33 +0800

    * Changed the playtest data so that the directory inside DropBox will be according to user

--- Assets/Scripts/Main/PlayTestManager.cs
+++ Assets/Scripts/Main/PlayTestManager.cs
@@ -19,7 +19,7 @@ public class PlayTestManager : MonoBehaviour {
             string username = "Karlo";
             string completionTime = "8888";
             string timeStamp = DateTime.Now.Month + "-" + DateTime.Now.Day + "-" + DateTime.Now.Year + "-" + DateTime.Now.Hour + DateTime.Now.Minute;
-            string fileData = Registry.map.currentLevel + "^" + timeStamp + "^" + completionTime + "^" + username + "^" + replayData;
+            string fileData = username + "^" + Registry.map.currentLevel + "^" + timeStamp + "^" + completionTime + "^" + replayData;
             string level = Registry.map.currentLevel;
             StartCoroutine(UploadData(level, fileData, username, timeStamp));
         }
@@ -30,9 +30,9 @@ public class PlayTestManager : MonoBehaviour {
         Debug.Log("sending " + fileData + "-" + timeStamp + "-" + username);
 
         WWWForm wwwForm = new WWWForm();
-        wwwForm.AddField("emailSubject", level);
-        wwwForm.AddField("emailTxt", level + "-" + username + "-" + timeStamp);
-        wwwForm.AddField("fileName", username + "-" + timeStamp + ".txt");
+        wwwForm.AddField("emailSubject", username);
+        wwwForm.AddField("emailTxt", username + "-" + level + "-" + timeStamp);
+        wwwForm.AddField("fileName", level + "-" + timeStamp + ".txt");
         wwwForm.AddField("fileData", fileData);
         WWW www = new WWW("http://www.accidentalrebel.com/minibots/playtestmailer.php", wwwForm);
         yield return www;

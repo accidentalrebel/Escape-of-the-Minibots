be320ac423c22b4153954c759463667e78df862b
ARebel-PC <ARebel-PC@localhost>
2014-04-19 23:44:08 +0800

    Can now set the TileFront for the different faces

--- Assets/Scripts/Sprites/TileFrontManager.cs
+++ Assets/Scripts/Sprites/TileFrontManager.cs
@@ -44,7 +44,7 @@ public class TileFrontManager : MonoBehaviour {
 		_tileSideDictionary.Add (TileSide.TOP_RIGHT, new Vector2(1, 1));
 		_tileSideDictionary.Add (TileSide.RIGHT, new Vector2(1, 0));
 		_tileSideDictionary.Add (TileSide.BOTTOM_RIGHT, new Vector2(1, -1));
-		_tileSideDictionary.Add (TileSide.BOTTOM, new Vector2(0, 1));
+		_tileSideDictionary.Add (TileSide.BOTTOM, new Vector2(0, -1));
 		_tileSideDictionary.Add (TileSide.BOTTOM_LEFT, new Vector2(-1, -1));
 		_tileSideDictionary.Add (TileSide.LEFT, new Vector2(-1, 0));
 		_tileSideDictionary.Add (TileSide.TOP_LEFT, new Vector2(-1, 1));
@@ -52,25 +52,58 @@ public class TileFrontManager : MonoBehaviour {
 
 	public void UpdateNeighbors () {
 
-		if ( GetLevelObjectAtDirection(TileSide.TOP) == null )
+		// TOP RIGHT
+		if ( GetTileObjectAtDirection(TileSide.TOP) != null
+			    && GetTileObjectAtDirection(TileSide.RIGHT) != null
+			    && GetTileObjectAtDirection(TileSide.TOP_RIGHT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.INWARD_TOP_RIGHT);
+		else if ( GetTileObjectAtDirection(TileSide.BOTTOM) != null
+		         && GetTileObjectAtDirection(TileSide.RIGHT) != null
+		         && GetTileObjectAtDirection(TileSide.BOTTOM_RIGHT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.INWARD_BOTTOM_RIGHT);
+		else if ( GetTileObjectAtDirection(TileSide.BOTTOM) != null
+		         && GetTileObjectAtDirection(TileSide.LEFT) != null
+		         && GetTileObjectAtDirection(TileSide.BOTTOM_LEFT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.INWARD_BOTTOM_LEFT);
+		else if ( GetTileObjectAtDirection(TileSide.TOP) != null
+		         && GetTileObjectAtDirection(TileSide.LEFT) != null
+		         && GetTileObjectAtDirection(TileSide.TOP_LEFT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.INWARD_TOP_LEFT);
+		else if ( GetTileObjectAtDirection(TileSide.TOP) == null
+		         && GetTileObjectAtDirection(TileSide.RIGHT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.TOP_RIGHT);
+		else if ( GetTileObjectAtDirection(TileSide.BOTTOM) == null
+		         && GetTileObjectAtDirection(TileSide.RIGHT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.BOTTOM_RIGHT);
+		else if ( GetTileObjectAtDirection(TileSide.BOTTOM) == null
+		         && GetTileObjectAtDirection(TileSide.LEFT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.BOTTOM_LEFT);
+		else if ( GetTileObjectAtDirection(TileSide.TOP) == null
+		         && GetTileObjectAtDirection(TileSide.LEFT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.TOP_LEFT);
+		else if ( GetTileObjectAtDirection(TileSide.TOP) == null )
 			_spriteManager.SetFrameTo("default", (int)TileSide.TOP);
-		else if ( GetLevelObjectAtDirection(TileSide.RIGHT) == null )
+		else if ( GetTileObjectAtDirection(TileSide.RIGHT) == null )
 			_spriteManager.SetFrameTo("default", (int)TileSide.RIGHT);
-		else if ( GetLevelObjectAtDirection(TileSide.BOTTOM) == null ) 
+		else if ( GetTileObjectAtDirection(TileSide.BOTTOM) == null ) 
 			_spriteManager.SetFrameTo("default", (int)TileSide.BOTTOM);
-		else if ( GetLevelObjectAtDirection(TileSide.LEFT) == null )
+		else if ( GetTileObjectAtDirection(TileSide.LEFT) == null )
 			_spriteManager.SetFrameTo("default", (int)TileSide.LEFT);
 		else
 			_spriteManager.SetFrameTo("default", (int)TileSide.MIDDLE);
 	}
 
-	private LevelObject GetLevelObjectAtDirection(TileSide tileSideDirection) {
+	private Tile GetTileObjectAtDirection(TileSide tileSideDirection) {
 
 		Vector2 directionOffset = _tileSideDictionary[tileSideDirection];
 		Vector3 _currentPosition = transform.parent.position;
 		Vector3 topPosition = new Vector3(_currentPosition.x + directionOffset.x
 		                                  , _currentPosition.y + directionOffset.y
 		                                  , _currentPosition.z);
-		return Registry.map.GetLevelObjectAtPosition(topPosition);
+		LevelObject levelObject = Registry.map.GetLevelObjectAtPosition(topPosition);
+		if ( levelObject is Tile && !(levelObject is HazardTile))
+			return (Tile)levelObject;
+		else
+			return null;
 	}
 }

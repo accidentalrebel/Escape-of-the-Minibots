d887901050f3d98aae6cf81929eded6df11679eb
ARebel-PC <ARebel-PC@localhost>
2014-04-19 22:50:41 +0800

    Created a funciton called GetLevelObjectAtDirection

--- Assets/Scripts/Sprites/TileFrontManager.cs
+++ Assets/Scripts/Sprites/TileFrontManager.cs
@@ -11,6 +11,22 @@ public class TileFrontManager : MonoBehaviour {
 		LEFT
 	};
 
+	private enum TileSide {
+		MIDDLE 			= 1,
+		TOP 			= 2,
+		TOP_RIGHT 		= 3,
+		RIGHT 			= 4,
+		BOTTOM_RIGHT 	= 5,
+		BOTTOM 			= 6,
+		BOTTOM_LEFT		= 7,
+		LEFT 			= 8,
+		TOP_LEFT 		= 9,
+		INWARD_TOP_RIGHT 	= 10,
+		INWARD_BOTTOM_RIGHT = 11,
+		INWARD_BOTTOM_LEFT 	= 12,
+		INWARD_TOP_LEFT 	= 13
+	}
+
 	private Direction4[] _noNeighborDirectionList;
 	private SpriteManager _spriteManager;
 	private Vector2 _currentPosition;
@@ -26,23 +42,35 @@ public class TileFrontManager : MonoBehaviour {
 	}
 
 	public void UpdateNeighbors () {
-		Vector3 _currentPosition = transform.parent.position;
 
-		Vector3 topPosition = new Vector3(_currentPosition.x, _currentPosition.y + 1, _currentPosition.z);
-		LevelObject levelObjectAtTop = Registry.map.GetLevelObjectAtPosition(topPosition);
-		if ( levelObjectAtTop == null || 
-		    ( levelObjectAtTop != null && !(levelObjectAtTop is Tile)))
-		{
-			if ( _currentPosition.x == 17 ) {			
-				if ( levelObjectAtTop == null )
-					Debug.Log ("At " + _currentPosition + " there is no top neighbor ");
-				else
-					Debug.Log ("At " + _currentPosition + " there is a top neighbor but is a " + levelObjectAtTop.GetType().ToString());
-			}
-
-			_spriteManager.SetFrameTo("default", 2);
-		}
+		if ( GetLevelObjectAtDirection(Direction4.TOP) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.TOP);
+		else if ( GetLevelObjectAtDirection(Direction4.RIGHT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.RIGHT);
+		else if ( GetLevelObjectAtDirection(Direction4.BOTTOM) == null ) 
+			_spriteManager.SetFrameTo("default", (int)TileSide.BOTTOM);
+		else if ( GetLevelObjectAtDirection(Direction4.LEFT) == null )
+			_spriteManager.SetFrameTo("default", (int)TileSide.LEFT);
 		else
-			_spriteManager.SetFrameTo("default", 1);
+			_spriteManager.SetFrameTo("default", (int)TileSide.MIDDLE);
+	}
+
+	private LevelObject GetLevelObjectAtDirection(Direction4 direction) {
+
+		Vector2 directionOffset = Vector2.zero;
+		if ( direction == Direction4.TOP )
+			directionOffset = new Vector2(0, 1);
+		else if ( direction == Direction4.RIGHT )
+			directionOffset = new Vector2(1, 0);
+		else if ( direction == Direction4.BOTTOM )
+			directionOffset = new Vector2(0, -1);
+		else if ( direction == Direction4.LEFT )
+			directionOffset = new Vector2(-1, 0);
+
+		Vector3 _currentPosition = transform.parent.position;
+		Vector3 topPosition = new Vector3(_currentPosition.x + directionOffset.x
+		                                  , _currentPosition.y + directionOffset.y
+		                                  , _currentPosition.z);
+		return Registry.map.GetLevelObjectAtPosition(topPosition);
 	}
 }

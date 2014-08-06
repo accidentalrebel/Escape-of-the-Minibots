bd0adffa229e90af398a886c8d12660f1e2f6aeb
ARebel-PC <karlo@accidentalrebel.com>
2014-08-07 00:59:53 +0800

    Can now calculate the topLeft position in CornerCover.cs

--- Assets/CornerCover.cs
+++ Assets/CornerCover.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class CornerCover : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public enum CornerLocation 
+	{
+		TopLeft,
+		TopRight,
+		BottomRight,
+		BottomLeft
+	};
+
+	public CornerLocation cornerLocation = CornerLocation.TopLeft; 
+	public Camera guiCamera;
+
+	void Start () 
+	{
+		repositionCornerCover();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void repositionCornerCover() 
+	{
+		if ( cornerLocation == CornerLocation.TopLeft ) {
+			var screenHeight = 4 * Camera.main.orthographicSize;
+			var screenWidth = screenHeight * Camera.main.aspect;
+			Vector3 coverSize = renderer.bounds.size;
+
+			gameObject.transform.localPosition = new Vector3(-screenWidth / 2 + coverSize.x / 2, screenHeight / 2 - coverSize.y / 2, -1);
+		}
 	}
 }

d2bd692a7c4f1752bc5ccde1cf1d5e335eb4194e
ARebel-PC <karlo@accidentalrebel.com>
2014-08-07 09:44:12 +0800

    LabelBackground script now automatically adds the labelBackground prefab

---
+++ Assets/LabelBackground.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TextMesh))]
+public class LabelBackground : MonoBehaviour {
+
+	public Object pfLabelBackground;
+
+	void Start () {
+		GameObject spawnedObject = (GameObject)Instantiate(pfLabelBackground);
+		spawnedObject.transform.parent = transform;
+		spawnedObject.transform.localPosition = new Vector3(0, 0, -0.5f);
+		spawnedObject.name = "labelBackground";
+	}
+}

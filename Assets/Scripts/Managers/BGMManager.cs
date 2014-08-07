e5939448e9beed03e2d1b5b27fa0f206da70dce4
ARebel-PC <karlo@accidentalrebel.com>
2014-08-07 12:18:13 +0800

    Created a BGM Manager

---
+++ Assets/Scripts/Managers/BGMManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMManager : MonoBehaviour {
+
+	Object[] _bgmList;
+
+	void Start()
+	{
+		_bgmList = Resources.LoadAll("Audio/BGM");
+
+		PlayBGM(0);
+	}
+
+	void PlayBGM(int trackNumber)
+	{
+		AudioClip audioToPlay = _bgmList[trackNumber] as AudioClip;
+		AudioSource.PlayClipAtPoint(audioToPlay, Camera.main.transform.position);
+	}
+}

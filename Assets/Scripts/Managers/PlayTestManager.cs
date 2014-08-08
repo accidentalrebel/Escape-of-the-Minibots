8d3d1347bfb3f7c14147f38e8471a9999055fcb9
AccidentalRebel <karlo@accidentalrebel.com>
2014-08-08 17:27:47 +0800

    We trace the returning WWWForm just in case there are any errors

--- Assets/Scripts/Managers/PlayTestManager.cs
+++ Assets/Scripts/Managers/PlayTestManager.cs
@@ -31,12 +31,16 @@ public class PlayTestManager : MonoBehaviour {
         Debug.Log("sending " + fileData + "-" + timeStamp + "-" + username);
 
         WWWForm wwwForm = new WWWForm();
-		wwwForm.AddField("emailSubject", username + "-" + level + "-" + timeStamp);
+		wwwForm.AddField("emailSubject", username);
         wwwForm.AddField("emailTxt", username + "-" + level + "-" + timeStamp);
         wwwForm.AddField("fileName", level + "-" + timeStamp + ".txt");
         wwwForm.AddField("fileData", fileData);
 		WWW www = new WWW("http://www.accidentalrebel.com/game-files/minibots/playtestmailer.php", wwwForm);
         yield return www;
-        Debug.Log("Uploaded replay data!");
-    }
+
+		if (!String.IsNullOrEmpty(www.error))
+			print("ERROR UPLOADING REPLAY: " + www.error);
+		else
+			print("Finished Uploading replay!");
+	}
 }
